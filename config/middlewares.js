0fec7e2810ec756b71983b9e5e0ea91a87e7fb2f
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2023-05-09 15:40:21 -0500

    CORS Configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,16 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://localhost:3000'],
+      methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
